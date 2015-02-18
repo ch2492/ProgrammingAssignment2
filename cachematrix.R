51c6ca1bfe3568ec078b8330ccff8aa3a3d1abd3
Chris Hintz <ch2492@att.com>
2015-02-18 11:36:01 -0800

    Update cachematrix.R
    Cleaning up the view.

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,10 @@
 ## Step 3 - Set the inverse of the Matrix
 ## Step 4 - Get the inverse of the Matrix
 ## makeCacheMatrix will build a list for cacheSolve
-
 makeCacheMatrix <- function(x = matrix()) {
 	inv_m = NULL  
-			
 	set = function(y) {
 		x <<- y 
-			
 		inv_m <<- NULL
 	}
 	get = function() x
@@ -19,22 +16,18 @@ makeCacheMatrix <- function(x = matrix()) {
 	list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
+## Compute the inverse
 ## Step 1 - Look at the Inverse
 ## Step 2 - Check to see if it has been calculated and not changed
 ## Step 3 - Retrieve the inverse from he cache
-
 cacheSolve <- function(x, ...) {
-        		
 	inv_m = x$getinv()
 	if (!is.null(inv_m)) {
 		message("getting cached data")
 		return(inv_m)
 		}
-			
 	data = x$get()
 	inv_m = solve(data,...)
-	
 	x$setinv(inv_m)
 	return(inv_m)
 }
